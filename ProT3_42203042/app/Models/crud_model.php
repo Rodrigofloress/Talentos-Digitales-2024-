9087c5f6848bb13392f4abaa65b4a6e9d1295e87
Rodrigo Flores <floresrodri223@gmail.com>
2024-07-22 00:12:37 -0300

    m

--- ProT3_42203042/app/Models/crud_model.php
+++ ProT3_42203042/app/Models/crud_model.php
@@ -6,5 +6,7 @@ use CodeIgniter\Model;
 
 class crud_model extends Model
 {
-
+    protected $table = 'usuarios';
+    protected $primaryKey = 'id_usuario';
+    protected $allowedFields = ['nombre', 'apellido', 'email', 'telefono', 'usuario', 'pass', 'perfil_id', 'baja', 'created_at'];
 }
